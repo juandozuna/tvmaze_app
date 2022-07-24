5bdf78f667fbb8055173345bd26e1214da624d94
Juan Daniel Ozuna <juandanielozuna2@gmail.com>
2022-07-24 19:22:01 -0400

    Added init provider
    Signed-off-by: Juan Daniel Ozuna <juandanielozuna2@gmail.com>

--- lib/injector.dart
+++ lib/injector.dart
@@ -3,8 +3,23 @@ import 'package:get_it/get_it.dart';
 final _injector = GetIt.instance;
 
 Future<void> _init() async {
-  // Injector.init();
+  _registerServices();
+  _registerRepositories();
+  _registerUseCases();
+  _registerProviders();
 }
+
+void _registerNetworkService() {
+
+}
+
+void _registerServices() {}
+
+void _registerRepositories() {}
+
+void _registerUseCases() {}
+
+void _registerProviders() {}
 
 /// Will get a registered type from the injector.
 T get<T>() => _injector<T>();
