1d5aa07dd7ad07192b72c5aba36777bce609c0fb
Juan Daniel Ozuna <juandanielozuna2@gmail.com>
2022-07-24 19:49:14 -0400

    created injector
    Signed-off-by: Juan Daniel Ozuna <juandanielozuna2@gmail.com>

--- lib/injector.dart
+++ lib/injector.dart
@@ -1,17 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 final _injector = GetIt.instance;
 
-Future<void> _init() async {
+List<SingleChildWidget> init() {
+  _initInjector();
+
+  final providers = [];
+
+  return providers.map((e) {
+    if (e is ChangeNotifier) return ChangeNotifierProvider.value(value: e);
+    return Provider.value(value: e);
+  }).toList();
+}
+
+void _initInjector() {
+  _registerNetworkService();
   _registerServices();
   _registerRepositories();
   _registerUseCases();
   _registerProviders();
 }
 
-void _registerNetworkService() {
-
-}
+void _registerNetworkService() {}
 
 void _registerServices() {}
 
