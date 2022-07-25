55b29f9cb276263e1c8146577671f07b802cae3f
Juan Daniel Ozuna <juandanielozuna2@gmail.com>
2022-07-25 08:15:45 -0400

    Loading
    Signed-off-by: Juan Daniel Ozuna <juandanielozuna2@gmail.com>

--- lib/presentation/splash_screen/splash_screen_page.dart
+++ lib/presentation/splash_screen/splash_screen_page.dart
@@ -24,8 +24,7 @@ class _SplashScreenPageState extends State<SplashScreenPage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Center(
-      child: Text('data'),
-    ));
+      body: CenterLoading(),
+    );
   }
 }
