1fd47f797f3aa9fe33a94efb080812106192f2b8
Juan Daniel Ozuna <juandanielozuna2@gmail.com>
2022-07-25 20:48:58 -0400

    Added code to navigate to detail page
    Signed-off-by: Juan Daniel Ozuna <juandanielozuna2@gmail.com>

--- lib/presentation/shows/show_list/show_list_item.dart
+++ lib/presentation/shows/show_list/show_list_item.dart
@@ -1,6 +1,9 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:tvmaze_app/injector.dart';
 import 'package:tvmaze_app/presentation/models/show_view_model.dart';
+import 'package:tvmaze_app/presentation/routes/route.dart';
+import 'package:tvmaze_app/presentation/show_detail/show_detail_provider.dart';
 import 'package:tvmaze_app/presentation/theme/app_theme.dart';
 
 class ShowListItem extends StatelessWidget {
@@ -12,6 +15,10 @@ class ShowListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: () {
+        get<ShowDetailProvider>().getShow(show.id);
+        Navigator.of(context).pushNamed(AppRoute.showDetails);
+      },
       tileColor: !isEven ? AppColors.oddListBackground : Colors.transparent,
       leading: Image(
         image: CachedNetworkImageProvider(show.posterImage),
